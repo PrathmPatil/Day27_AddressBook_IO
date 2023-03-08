d6f5a4e15632b92b98b9e37e7f161eb44113705d
PrathmPatil <prathmpatil2818@gmail.com>
2023-03-08 16:46:57 +0530

    Edit contact by using first name

--- src/com/addressbook/Main.java
+++ src/com/addressbook/Main.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
-
-	 static Contacts contact;
+	static Contacts contact;
+	  static String name;
+	  Scanner sc=new Scanner(System.in);
 	    static ArrayList<Contacts> contacts = new ArrayList<Contacts>();
 	    static Main addressbook=new Main();
 	    public void uc1_createContact(){
@@ -32,20 +32,88 @@ public class Main {
 	    public void uc2_addContact() {
 	        contacts.add(contact);
 	    }
+	    public void uc3_editContact(){
+	      
+	        System.out.println("Enter the name ");
+	        name=sc.next();
+	        if(contacts.get(0).getFirstName().equalsIgnoreCase(name))
+	            {
+	                System.out.println("Select which one you want to edit");
+	                System.out.println("1.First Name\n" +
+	                        "2.Last Name\n" +
+	                        "3.Address\n" +
+	                        "4.City\n" +
+	                        "5.State\n" +
+	                        "6.Zip\n" +
+	                        "7.Phone Number\n" +
+	                        "8.Email Id\n");
+	                        
+	                int option = sc.nextInt();
+	                switch(option)
+	                {
+	                    case 1:
+	                        System.out.println("Change first name");
+	                        contacts.get(0).setFirstName(sc.next());
+	                        break;
+	                    case 2:
+	                        System.out.println("Change Last name");
+	                        contacts.get(0).setLastName(sc.next());
+	                        break;
+	                    case 3:
+	                        System.out.println("Change Address name");
+	                        contacts.get(0).setAddress(sc.next());
+	                        break;
+	                    case 4:
+	                        System.out.println("Change City name");
+	                        contacts.get(0).setCity(sc.next());
+	                        break;
+	                    case 5:
+	                        System.out.println("Change State name");
+	                        contacts.get(0).setState(sc.next());
+	                        break;
+	                    case 6:
+	                        System.out.println("Change Zip");
+	                        contacts.get(0).setZip(sc.next());
+	                        break;
+	                    case 7:
+	                        System.out.println("Change Phone number");
+	                        contacts.get(0).setPhoneNumber(sc.next());
+	                        break;
+	                    case 8:
+	                        System.out.println("Change Email ID");
+	                        contacts.get(0).setEmail(sc.next());
+	                        break;
+	                    default:
+	                        System.out.println("Select correct option");
+	                }
+	            }
+	            else
+	            {
+	                System.out.println("Cannot find name..."+"\n Please enter valid name.");
+	                uc3_editContact();
+	            }
+	        
+	        }
 	    public static void main() {
-	    	System.out.println("Enter \n 1.Add Contact");
+	    	System.out.println("Enter \n 1.Add Contact \n2.Edit Contact \n3.Display");
 	    	Scanner sc=new Scanner(System.in);
 	    	int choose=sc.nextInt();
 	    	switch(choose)
 	    	{
 	    	case 1:
-         	addressbook.uc1_createContact();
-         	addressbook.uc2_addContact();
-         	main();
-             break;
-         default:
-             	System.out.println("Please enter valid number.");
-             	break;
+          	addressbook.uc1_createContact();
+          	addressbook.uc2_addContact();
+          	main();
+              break;
+	    	case 2:
+	    		addressbook.uc3_editContact();
+	    		main();
+              break;
+	    	case 3:
+	    		System.out.println(contacts);
+          default:
+              	System.out.println("Please enter valid number.");
+              	break;
 	    		
 	    	}
 	    }
